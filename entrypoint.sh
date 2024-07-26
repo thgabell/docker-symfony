ca193d64cb84ee021c400ae95cf89f7ea891d46f
thgabell <thgabell@student.42.fr>
2024-07-27 00:01:35 +0200

    dev toolbar fixed

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,9 @@ if [ "$(ls -A /var/www/html)" ]; then
     echo "Directory not empty, symfony project creation skipped..."
 else
     echo "Symfony project creation...: ${PROJECT_NAME}"
-    symfony new ${PROJECT_NAME} --webapp --no-git 
+    create-project symfony/skeleton:"7.1.*" ${PROJECT_NAME}
+    cd ${PROJECT_NAME}
+    composer require webapp 
     mv ${PROJECT_NAME}/* ${PROJECT_NAME}/.* .
     rmdir ${PROJECT_NAME}
     composer install
